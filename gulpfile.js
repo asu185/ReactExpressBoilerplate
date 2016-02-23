47bc9f067156120fc999f78843d58d2e0eb0c3d1
yuhsuan <frorgr@hotmail.com>
2016-02-23 13:53:43 +0800

    Add hot-reloading & notifier

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,32 @@ var watchify = require('watchify');
 var reactify = require("reactify");
 var source = require("vinyl-source-stream");
 var nodemon = require('gulp-nodemon');
+var notifier = require('node-notifier');
+
+var notify = function(error) {
+  var message = 'In: ';
+  var title = 'Error: ';
+
+  if(error.description) {
+    title += error.description;
+  } else if (error.message) {
+    title += error.message;
+  }
+
+  if(error.filename) {
+    var file = error.filename.split('/');
+    message += file[file.length-1];
+  }
+
+  if(error.lineNumber) {
+    message += '\nOn Line: ' + error.lineNumber;
+  }
+
+  notifier.notify({title: title, message: message});
+};
 
 var bundler = watchify(browserify({
-  entries: ['./public/main.jsx'],
+  entries: ['./public/components/main.jsx'],
   transform: [reactify],
   extensions: ['.jsx'],
   debug: true
@@ -15,8 +38,9 @@ var bundler = watchify(browserify({
 function bundle() {
   return bundler
     .bundle()
+    .on('error', notify)
     .pipe(source('main.js'))
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest('public'));
 }
 bundler.on('update', bundle);
 
@@ -27,10 +51,29 @@ gulp.task('build', function() {
 gulp.task('serve', function() {
   nodemon({ 
     script: 'server/server.js', 
+    ignore: [
+      './public',
+      './omlet-autotest'
+    ],
     ext: 'html js'
   });
 });
 
-gulp.task("default",["build", "serve"], function(){
-   console.log("Gulp completed..."); 
+// gulp.task("default", ['build', 'serve'], function(){
+//   console.log("Gulp completed..."); 
+// });
+
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+    gulp.src('./public')
+      .pipe(webserver({
+        livereload: true,
+        directoryListing: true,
+        open: true
+      }));
+});
+
+gulp.task("default", ['webserver', 'build', 'serve'], function() {
+  console.log("Gulp completed...");
 });
